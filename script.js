ee88b1d0c15ae5ad9b85fb271ef1e03c61fa116e
maurice <filiatreaultmaurice@gmail.com>
2022-01-22 18:45:54 -0500

    Change the value of the offset for mobile

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function getDragAfterElementMobile(container, y) {
   return draggableElements.reduce(
     (closest, child) => {
       const box = child.getBoundingClientRect();
-      const offset = y - 120 - box.top - box.height / 2;
+      const offset = y - 110 - box.top - box.height / 2;
       if (offset < 0 && offset > closest.offset) {
         return { offset: offset, element: child };
       } else {
